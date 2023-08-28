ab6b73a27596947053a74474a7c4681f8a1bb962
Siri2411 <83855091+Siri2411@users.noreply.github.com>
2023-08-28 20:15:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import random
+import os
+from PIL import Image
+import time
+import imutils
+from tensorflow.keras.models import load_model
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+net = cv2.dnn.readNet("yolov3-custom_7000.weights", "yolov3-custom.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+
+model = load_model('helmet-nonhelmet_cnn.h5')
+print('model loaded!!!')
+
